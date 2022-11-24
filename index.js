3664a4f9f0b22965567d8cfa44efce082609d226
ajkn1904 <ajknishat@gmail.com>
2022-11-24 17:47:58 +0600

    registration done

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 
 const app = express();
 
-
+//middleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7splzic.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+        const usersCollection = client.db('secondLook').collection('Users');
+
+
+        //user 
+        app.post('/users', async(req, res) => {
+            const users = req.body
+            const result = await usersCollection.insertOne(users)
+            res.send(result)
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.log);
+
+
 
 
 app.get('/', async(req, res) => {
